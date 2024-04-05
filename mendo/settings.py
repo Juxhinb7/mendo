e95a0f6bc10cc08759a1b4607ac367f275519a77
juxhinb7 <juxhinbazelli@gmail.com>
2024-04-05 20:02:10 +0200

    changed allowed hosts

--- mendo/settings.py
+++ mendo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ondigitalocean.app']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
